abbbc1fa959ef104f87a7f992dffdadacee06285
mrezkov <mrezkov@phoenixit.ru>
2023-11-13 21:34:22 +0300

    ap input styles

--- src/shared/input/ui/ApInput.tsx
+++ src/shared/input/ui/ApInput.tsx
@@ -1,15 +1,23 @@
 import { FC, PropsWithChildren } from "react";
 import styleClasses from "./ApInput.module.scss";
+import { classNames } from "../../utils/classNames";
 
 interface IApInputProps {
   placeholder?: string;
   value: string;
+  additionalClasses?: string[];
+  disable?: boolean;
 };
 
 const ApInput: FC<IApInputProps> = (props: PropsWithChildren<IApInputProps>) => {
-  const { placeholder = '', value } = props;
+  const { placeholder = '', value, additionalClasses = [], disable = false } = props;
+  const className = classNames({
+    mainClassName: styleClasses.ApInput,
+    additional: additionalClasses,
+  }); 
+
   return (
-    <input className={styleClasses.ApInput} placeholder={placeholder} value={value}/>
+    <input className={className} placeholder={placeholder} value={value} disabled={disable}/>
   )
 };
 
