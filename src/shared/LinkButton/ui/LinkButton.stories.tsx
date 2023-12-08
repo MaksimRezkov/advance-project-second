0bbf5bd3ed54ed28e83bdcad009466848d4b2b71
mrezkov <mrezkov@phoenixit.ru>
2023-12-08 22:01:32 +0300

    link button story

---
+++ src/shared/LinkButton/ui/LinkButton.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { LinkButton } from './LinkButton';
+import { ThemeDecorator } from 'shared/config/storybook/StyleDecorator/ThemeDecorator';
+import { RouteDecorator } from 'shared/config/storybook/StyleDecorator/RouteDecorator';
+
+const meta = {
+  title: 'shared/LinkButton',
+  component: LinkButton,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof LinkButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const LinkLight: Story = {
+  args: {
+    to: '',
+    children: 'App Link',
+  },
+  decorators: [
+    RouteDecorator,
+  ],
+};
+
+export const LinkDark: Story = {
+  args: {
+    to: '',
+    children: 'App Link',
+  },
+  decorators: [
+    RouteDecorator,
+    ThemeDecorator('dark'),
+  ],
+};
