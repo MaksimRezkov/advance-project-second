b9982218ef6a07d6e4f58f4842af329481e8eec8
mrezkov <mrezkov@phoenixit.ru>
2024-01-04 21:43:52 +0300

    Input story

---
+++ src/shared/input/ui/ApInput.stories.ts
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ApInput } from './ApInput';
+import { ThemeDecorator } from 'shared/config/storybook/StyleDecorator/ThemeDecorator';
+
+const meta = {
+  title: 'shared/ApInput',
+  component: ApInput,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof ApInput>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const InputLight: Story = {
+  args: {
+    placeholder: 'Story Input',
+  },
+  decorators: [
+    ThemeDecorator('light'),
+  ],
+};
+
+export const InputDisabled: Story = {
+  args: {
+    placeholder: 'Story Input Disabled',
+    disable: true,
+  },
+  decorators: [
+    ThemeDecorator('light'),
+  ],
+};
+
+export const InputDark: Story = {
+  args: {
+    placeholder: 'Story Input',
+  },
+  decorators: [
+    ThemeDecorator('dark'),
+  ],
+};
